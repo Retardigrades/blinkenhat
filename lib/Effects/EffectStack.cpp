45d85671f3805a165e926c60a9ca41a9e01f828b
Jan Losinski <losinski@wh2.tu-dresden.de>
2018-02-14 16:27:03 +0100

    Integreate Twinkle effect in Effect Stack.
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- lib/Effects/EffectStack.cpp
+++ lib/Effects/EffectStack.cpp
@@ -6,6 +6,7 @@
 
 #include "ERainbow.h"
 #include "EDot.h"
+#include "ETwinkle.h"
 
 Effect *EffectStack::createEffect(const HatConfig::EffectCfg &cfg) {
   if (cfg.type()==F("rainbow")) {
@@ -15,6 +16,11 @@ Effect *EffectStack::createEffect(const HatConfig::EffectCfg &cfg) {
   }
   if (cfg.type()==F("dot")) {
     Effect *fx = new EDot();
+    fx->config(cfg.config());
+    return fx;
+  }
+  if (cfg.type()==F("twinkle")) {
+    Effect *fx = new ETwinkle();
     fx->config(cfg.config());
     return fx;
   }
