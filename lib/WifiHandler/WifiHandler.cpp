9bfb8edee9df95caee2ee806dea7696039e60cdc
Jan Losinski <losinski@wh2.tu-dresden.de>
2018-02-20 17:59:34 +0100

    Wifi: select channel with the least wifis.
    This is a try to select the channel with the least wifis on it for AP
    mode operation. This might give the wifi a better stability. The
    default is always channel 1 in the arduino-wrapper for the ESP.
    
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- lib/WifiHandler/WifiHandler.cpp
+++ lib/WifiHandler/WifiHandler.cpp
@@ -1,5 +1,6 @@
 #include <ESP8266WiFi.h>
 #include <user_interface.h>
+#include <array>
 
 #include "WifiHandler.h"
 
@@ -16,6 +17,16 @@ void WifiHandler::configure(const HatConfig &cfg) {
 
   timeout = dcfg.getOption(F("ap_timeout"), 0U);
 }
+
+
+struct ChannelStat {
+  uint8_t channel;
+  uint8_t count;
+
+  ChannelStat(uint8_t channel) : channel(channel), count(0) {}
+  bool operator<(const ChannelStat& other) { return count < other.count; }
+};
+
 
 bool WifiHandler::connect() {
   last_client = millis();
@@ -35,10 +46,42 @@ bool WifiHandler::connect() {
   }
 
   if (ap_ssid.length() > 0 && ap_passwd.length() > 0) {
+
+    int channel = 1;
+
+    uint8_t scan_results = 0;
+
+    // find channel with min networks.
+    if ((scan_results = WiFi.scanNetworks()) > 0) {
+
+      std::array<ChannelStat, 4> stats{{1, 5, 9, 13}};
+      for (uint8_t idx = 0; idx < scan_results; ++idx) {
+        int current = WiFi.channel(idx);
+        for (auto& stat : stats) {
+          if (stat.channel == current) {
+            stat.count ++;
+            break;
+          }
+        }
+      }
+
+      const auto& minCh = std::min_element(stats.begin(), stats.end());
+      channel = minCh->channel;
+
+#ifdef DEBUG_ESP_WIFI
+      Serial.println("Channel stats:");
+      for (const auto& stat : stats) {
+        Serial.printf("  %02d: %d\n", stat.channel, stat.count);
+      }
+      Serial.printf("Selected channel: %d", channel);
+#endif
+    }
+
     WiFi.mode(WIFI_AP);
 
     WiFi.softAP((ap_ssid + String(ESP.getChipId(), HEX)).c_str(),
-                ap_passwd.c_str());
+                ap_passwd.c_str(),
+                channel);
 
     delay(500);
     Serial.println(WiFi.softAPIP());
